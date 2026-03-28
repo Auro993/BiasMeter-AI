700f68572aafcd92d7ea0308584f9c37defcfc69
Aurosmita Sahoo <sahooaurosmita204@gmail.com>
2026-03-28 10:49:39 +0530

    Update WebConfig.java

--- src/main/java/com/example/biasmeter/config/WebConfig.java
+++ src/main/java/com/example/biasmeter/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.biasmeter.config;  
+package com.example.biasmeter.config;   
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
