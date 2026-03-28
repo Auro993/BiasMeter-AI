4162b5ccce2bd9c416390a5331b108214bbac230
Aurosmita Sahoo <sahooaurosmita204@gmail.com>
2026-03-28 10:50:25 +0530

    Update AnalyticsController.java

--- src/main/java/com/example/biasmeter/controller/AnalyticsController.java
+++ src/main/java/com/example/biasmeter/controller/AnalyticsController.java
@@ -1,4 +1,4 @@
-package com.example.biasmeter.controller;
+package com.example.biasmeter.controller; 
  
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
